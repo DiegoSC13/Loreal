326adc97a785f5132a3af9e6a79caa3f77c1eb87
Diego Silvera Coeff <diegosc1313@gmail.com>
2026-03-11 12:10:03 +0100

    Minor changes

--- utils.py
+++ utils.py
@@ -3,15 +3,17 @@ import os
 def generar_lista_tif(directorio, archivo_salida="lista_tif.txt"):
     lista_tif = []
 
-    for root, dirs, files in os.walk(directorio):
+    for root, dirs, files in os.walk(directorio, followlinks=True):
+        # evitar entrar en directorios llamados "check"
+        dirs[:] = [d for d in dirs if d != "check"]
+
         for file in files:
             if file.lower().endswith(".tif"):
-                ruta_completa = os.path.join(root, file)
-                lista_tif.append(ruta_completa)
+                lista_tif.append(os.path.join(root, file))
 
+    lista_tif.sort()
     with open(archivo_salida, "w") as f:
         for ruta in lista_tif:
             f.write(ruta + "\n")
 
     print(f"Se encontraron {len(lista_tif)} archivos .tif")
-    print(f"Lista guardada en: {archivo_salida}")
